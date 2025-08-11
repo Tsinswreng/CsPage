ca41d6ae641499cae516d399bd2a6454d8e26a73
Tsinswreng <Tsinswreng@gmail.com>
2025-08-11 19:05:13 +0800

    文件更名

---
+++ proj/Tsinswreng.CsPage/I_HasTotCnt.cs
@@ -0,0 +1,9 @@
+namespace Tsinswreng.CsPage;
+
+
+/// <summary>
+/// 分頁查詢ʹ果中 是否有總記錄數
+/// </summary>
+public partial interface I_HasTotCnt{
+	public bool HasTotCnt{get;set;}
+}
--- proj/Tsinswreng.CsPage/I_WantTotCnt.cs
+++ proj/Tsinswreng.CsPage/I_WantTotCnt.cs
@@ -6,10 +6,3 @@ namespace Tsinswreng.CsPage;
 public partial interface I_WantTotCnt{
 	public bool WantTotCnt{get;set;}
 }
-
-/// <summary>
-/// 分頁查詢ʹ果中 是否有總記錄數
-/// </summary>
-public partial interface I_HasTotCnt{
-	public bool HasTotCnt{get;set;}
-}
